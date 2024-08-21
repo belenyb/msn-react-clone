cd3170a79126f9e6c6382f7f9b3d5bc6337b8e72
Belen Yarde Buller <belenyardebuller@gmail.com>
2024-08-21 17:14:55 -0300

    First repo push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import LoginWindow from './components/LoginWindow';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [showLoginWindow, setShowLoginWindow] = useState(false);
+
+  const toggleLoginWindow = (isVisible) => {
+    setShowLoginWindow(isVisible);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="windows-app-icons position-absolute top-0 start-0 p-2">
+        <div className='d-flex flex-column align-items-center mb-2 p-2'>
+          <img src="/images/recycle-bin.webp" alt="Windows recycle bin" width="50" />
+          <span className='text-white'>Recycle Bin</span>
+        </div>
+        <div className='d-flex flex-column align-items-center p-2' onDoubleClick={() => toggleLoginWindow(true)}>
+          <img src="/images/msn.webp" alt="Windows Live Messenger icon" width="50" />
+          <span className='text-white text-center lh-1'>Windows Live <br /> Messenger</span>
+        </div>
+      </div>
+      {showLoginWindow && <LoginWindow onClose={() => toggleLoginWindow(false)} />}
     </div>
   );
 }
